4eeb61281f30cfb7f6f1c7b7a066eecf4c6cb5de
Louis Gerber <louisgerber004@gmail.com>
2023-05-30 20:21:49 +0200

    dernier commit en date

--- FrontEnd/administrator-page.js
+++ FrontEnd/administrator-page.js
@@ -1,5 +1,7 @@
+
 let token = window.localStorage.getItem("token")
-if (token !== null){
+console.log(token)
+if (token !== null && token !== undefined){
     console.log("token found")
     const adminMode = document.querySelector(".adminmode")
     adminMode.classList.remove("no_display")
@@ -9,7 +11,6 @@ if (token !== null){
 //open module 
 const editionClick = document.querySelector(".edition_mode")
 editionClick.addEventListener("click", function(){
-    console.log("hello")
     let modaleWindow = document.querySelector(".module")
     modaleWindow.classList.remove("no_display") 
 })
@@ -24,9 +25,13 @@ closeIcone.addEventListener("click", function(){
 
 
 
-//add project to module
+
+
+//add/del project to module
 const reponseWorks = await fetch("http://localhost:5678/api/works")
 const works = await reponseWorks.json()
+
+
 const photoContainer = document.querySelector(".photo_container")
 
 function create(works) {
@@ -36,20 +41,113 @@ function create(works) {
         let img = document.createElement("img")
         img.src = works[i].imageUrl
         img.alt = works[i].title
+        let imgId = works[i].id
+        console.log(imgId)
+    
         let caption = document.createElement("figcaption")
         caption.innerText = "éditer"
-
         let trashIcon = document.createElement("div")
         trashIcon.classList.add("trashIconContainer")
         trashIcon.innerHTML =`<i class="fa-solid fa-trash-can trashIcon"></i>`
+        trashIcon.classList.add(`referenceId${imgId}`)
+
         imgContainer.appendChild(trashIcon)
         imgContainer.appendChild(img)
         imgContainer.appendChild(caption)
+
+        let delBtn = document.querySelector(`.referenceId${imgId}`)
+        delBtn.addEventListener("click", function(){
+            let deleteInfos = {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Autorization": `Bearer ${token}` 
+                },
+            }
+            fetch(`http://localhost:5678/api/works/${imgId}`, deleteInfos)
+        })
+
     }
 }
 create(works)
 
+
+
+
+
+//Navigation entre les fenetre du module
+
+
 const addPhoto = document.querySelector(".addPhoto")
+const addPhotoBlock = document.querySelector(".addphoto_block")
+const backArrow = document.querySelector(".back_arrow")
+const delPhotoBlock = document.querySelector(".delphoto_block")
+
+
 addPhoto.addEventListener("click", function(){
-    
+    delPhotoBlock.classList.add("no_display")
+    backArrow.classList.remove("no_display")
+    addPhotoBlock.classList.remove("no_display")
+})
+
+backArrow.addEventListener("click", function(){
+    delPhotoBlock.classList.remove("no_display")
+    backArrow.classList.add("no_display")
+    addPhotoBlock.classList.add("no_display")
+})
+
+const form1 = document.querySelector(".fm1")
+const form2 = document.querySelector(".fm2")
+const validationBtn = document.querySelector(".add_photo_validation_btn")
+
+form2.addEventListener("input", function(){
+    if (form1.value !== null && form2.value !== null) {
+        validationBtn.classList.add("selected")    
+    } else {
+        validationBtn.classList.remove("selected")
+    }
+})
+
+
+
+
+
+
+const reponseWorksCategories = await fetch("http://localhost:5678/api/categories")
+const worksCategories = await reponseWorksCategories.json()
+console.log(worksCategories)
+// ajout des catégories dans le menu déroulant de l'ajout de projets
+
+for (let i = 0; i < worksCategories.length; i++){
+    let options = document.createElement("option")
+    options.value = worksCategories[i].id
+    options.innerHTML = worksCategories[i].name
+    form2.appendChild(options)
+}
+
+
+
+
+// Ajout des photos
+
+
+
+validationBtn.addEventListener("click", function(){
+    if (form1.value !== null && form2.value !== null) {
+        let newId = worksCategories[-1].id - 1
+        let content = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Autorization": `${token}` 
+            },
+            body: {
+                "id": newId,
+                "title": form1.value,
+                "imageUrl": "string",
+                "categoryId": form2.value,
+                "userId": 1
+            }
+        }
+    } 
 })
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -6,14 +6,12 @@ const passwordInput = document.getElementById("password")
 btnLogin.addEventListener("click", async function(){
     let email = emailInput.value
     let password = passwordInput.value
-    console.log(email)
-    console.log(password)
     const reponse = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         body: (`{"email": "${email}","password": "${password}"}`),
         headers: { "Content-Type": "application/json" }
     })
     const data = await reponse.json()
-    window.localStorage.setItem("token", data)
+    window.localStorage.setItem("token", data.token)
     window.location.href = "index.html"
 })
